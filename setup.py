cf27e70c1fc456230e5ddbdfe700bac8c1143b36
tatsuro-yasukawa <tatsuro-yasukawa@cookpad.com>
2018-10-30 14:51:22 +0900

    Fix Not found file error

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ swig_opts = ['-shadow', '-c++']
 swig_opts.extend("-I"+d for d in inc_dir)
 
 setup(name = "mecab-python3",
-    version = '0.8.2',
+    version = '0.8.3',
     description = 'python wrapper for mecab: Morphological Analysis engine',
     long_description= read_file('README.rst'),
     maintainer = 'Tatsuro Yasukawa',
