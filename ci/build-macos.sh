5dfe5d5acb4f691a00b044dea17f4b2d08953d6a
Aki Ariga <chezou+github@gmail.com>
2018-11-14 01:16:19 +0900

    Add import test

--- ci/build-macos.sh
+++ ci/build-macos.sh
@@ -10,9 +10,12 @@ PYTHON_VERSIONS=(2.7.15 3.4.9 3.5.6 3.6.7 3.7.1)
 for PYTHON in ${PYTHON_VERSIONS[@]}; do
     pyenv install $PYTHON
     PYTHON_PATH="$(pyenv root)/versions/${PYTHON}/bin"
-    ${PYTHON_PATH}/pip install -U setuptools
-    ${PYTHON_PATH}/pip install wheel
-    ${PYTHON_PATH}/python setup.py bdist_wheel
+    "${PYTHON_PATH}/pip" install -U pip
+    "${PYTHON_PATH}/pip" install -U setuptools
+    "${PYTHON_PATH}/pip" install wheel
+    "${PYTHON_PATH}/python" setup.py bdist_wheel
+    "${PYTHON_PATH}/pip" install mecab-python3 --no-index -f dist/
+    "${PYTHON_PATH}/python" -c "import MeCab"
 done
 
 $(pyenv root)/versions/${PYTHON}/bin/python setup.py sdist
