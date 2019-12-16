772f4bfa802522309d347cabf0594fc55586dff3
Alexey Korobko <akkorobk@amazon.com>
2019-12-16 17:48:34 +0000

    Upgrade SWIG to 4.x in order to support memory management on python 3.8

--- scripts/build_requirements.py
+++ scripts/build_requirements.py
@@ -4,11 +4,11 @@
 
 from utils import Downloadable
 
-POOL_URL = "https://deb.debian.org/debian/pool/main"
+POOL_URL = "https://sourceforge.net/projects"
 
 SWIG = Downloadable(
-    name          = "swig-3.0.12.tar.gz",
-    unpacked_name = "swig-3.0.12",
-    url  = POOL_URL + "/s/swig/swig_3.0.12.orig.tar.gz",
-    hash = "7cf9f447ae7ed1c51722efc45e7f14418d15d7a1e143ac9f09a668999f4fc94d"
+    name          = "swig-4.0.1.tag.gz",
+    unpacked_name = "swig-4.0.1",
+    url  = POOL_URL + "/swig/files/swig/swig-4.0.1/swig-4.0.1.tar.gz/download",
+    hash = "7a00b4d0d53ad97a14316135e2d702091cd5f193bb58bcfcd8bc59d41e7887a9"
 )
