64d033ab4ece7ec500773f8ad2cf951f3dd6ff07
Alexey Korobko <32317929+akorobko@users.noreply.github.com>
2019-12-19 09:02:30 -0800

    Fixed the typo.
    Does not really affect anything since it is just temporary file name

--- scripts/build_requirements.py
+++ scripts/build_requirements.py
@@ -7,7 +7,7 @@ from utils import Downloadable
 POOL_URL = "https://sourceforge.net/projects"
 
 SWIG = Downloadable(
-    name          = "swig-4.0.1.tag.gz",
+    name          = "swig-4.0.1.tar.gz",
     unpacked_name = "swig-4.0.1",
     url  = POOL_URL + "/swig/files/swig/swig-4.0.1/swig-4.0.1.tar.gz/download",
     hash = "7a00b4d0d53ad97a14316135e2d702091cd5f193bb58bcfcd8bc59d41e7887a9"
