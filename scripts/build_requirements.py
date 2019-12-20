deb9ad47b6e4a5c1b3fc756915f68bb800e4d051
Alexey Korobko <32317929+akorobko@users.noreply.github.com>
2019-12-19 16:01:16 -0800

    Changed download URL to the direct one

--- scripts/build_requirements.py
+++ scripts/build_requirements.py
@@ -4,11 +4,11 @@
 
 from utils import Downloadable
 
-POOL_URL = "https://sourceforge.net/projects"
+POOL_URL = "http://prdownloads.sourceforge.net"
 
 SWIG = Downloadable(
     name          = "swig-4.0.1.tar.gz",
     unpacked_name = "swig-4.0.1",
-    url  = POOL_URL + "/swig/files/swig/swig-4.0.1/swig-4.0.1.tar.gz/download",
+    url  = POOL_URL + "/swig/swig-4.0.1.tar.gz",
     hash = "7a00b4d0d53ad97a14316135e2d702091cd5f193bb58bcfcd8bc59d41e7887a9"
 )
