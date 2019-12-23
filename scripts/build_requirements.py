0a45e3c119711fa0aff20a533f0c85e514195d01
Alexey Korobko <32317929+akorobko@users.noreply.github.com>
2019-12-23 10:29:09 -0800

    bypassing Flake8 restrictions

--- scripts/build_requirements.py
+++ scripts/build_requirements.py
@@ -9,6 +9,9 @@ POOL_URL = "https://src.fedoraproject.org/lookaside/pkgs"
 SWIG = Downloadable(
     name          = "swig-4.0.1.tar.gz",
     unpacked_name = "swig-4.0.1",
-    url  = POOL_URL + "/swig/swig-4.0.1.tar.gz/sha512/595ef01cb83adfa960ceed9c325a9429192549e8d1e9aa3ab35a4301512a61d82e2e89a8c7939c2a5a0811254ea1832a443bd387e11459eb2b0bafc563ad1308/swig-4.0.1.tar.gz",
+    url  = POOL_URL + "/swig/swig-4.0.1.tar.gz/sha512/" +
+        "595ef01cb83adfa960ceed9c325a9429192549e8d1e9aa3ab35a" +
+        "4301512a61d82e2e89a8c7939c2a5a0811254ea1832a443bd387" +
+        "e11459eb2b0bafc563ad1308/swig-4.0.1.tar.gz",
     hash = "7a00b4d0d53ad97a14316135e2d702091cd5f193bb58bcfcd8bc59d41e7887a9"
 )
