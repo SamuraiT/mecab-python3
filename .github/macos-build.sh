e446ad3d2ea819ff412a72e2e5695f7f444c1dbf
Paul O'Leary McCann <polm@dampfkraft.com>
2023-09-14 21:33:54 +0900

    No requirements.txt

--- .github/macos-build.sh
+++ .github/macos-build.sh
@@ -52,6 +52,5 @@ cd ../..
 
 python -m pip install --upgrade setuptools wheel pip setuptools-scm
 python -m pip install cibuildwheel==2.14.1
-pip install -r requirements.txt
 
 python -m cibuildwheel --platform macos --archs x86_64,arm64,universal2 --output-dir dist
