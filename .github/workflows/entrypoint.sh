a3df2ffd351e2ea832b2ef9e7c79325b600eaf22
Paul O'Leary McCann <polm@dampfkraft.com>
2023-12-21 23:56:45 +0900

    Add 3.12 in the right place

--- .github/workflows/entrypoint.sh
+++ .github/workflows/entrypoint.sh
@@ -18,7 +18,7 @@ make install
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
 
 # Build the wheels
-for PYVER in cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310 cp311-cp311; do
+for PYVER in cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310 cp311-cp311 cp312-cp312; do
   # build the wheels
   /opt/python/$PYVER/bin/pip wheel /github/workspace -w /github/workspace/wheels || { echo "Failed while buiding $PYVER wheel"; exit 1; }
 done
